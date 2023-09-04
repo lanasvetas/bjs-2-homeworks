b1f19dbe0a5929c5fc926a1c7e0b7bf52384dbf6
lanasvetas <spilana98@yahoo.com>
2023-09-04 11:14:11 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,23 @@
 function compareArrays(arr1, arr2) {
-  
-}
+    let string1 = JSON.stringify(arr1);
+    let string2 = JSON.stringify(arr2);
+    if (string1 !== string2) {
+        return false;
+    }
+    return arr1.every((element, i) => { return element===arr2[i]});
+
+};
 
 function getUsersNamesInAgeRange(users, gender) {
-  
-}
+
+    let result = users.filter (users => {
+        return users.gender==gender;
+    })
+    if (result.length === 0) {
+        return 0;
+    }
+    let result2 = result.reduce((acc, result) => {
+        return acc+ result.age
+    },0) 
+        return result2/ result.length;
+};
